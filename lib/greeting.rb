4bd109a237213490b4ce130bb874c289f117db13
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-13 19:32:10 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,10 +1,5 @@
 # code the #greeting method here!
 
-puts "Greetings, enter login:" 
-end
-
-def Greetings(#{gets.strip}) 
-name = (#{gets.strip})
-
-puts "Welcome, " + ($stdout) + "."
-
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end 
